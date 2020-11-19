8081361a89b68909adca149fae6667834b082891
swethaeprm <74707581+swethaeprm@users.noreply.github.com>
2020-11-19 15:05:18 +0530

    Add files via upload

---
+++ precache-manifest.dd77a815e89aed240d3f88cd0572afa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f03d55aa3205bb792689",
+    "url": "/static/css/main.62a3ae10.chunk.css"
+  },
+  {
+    "revision": "f03d55aa3205bb792689",
+    "url": "/static/js/main.f03d55aa.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c347a5d08e65d5e7c359",
+    "url": "/static/css/2.c9edb238.chunk.css"
+  },
+  {
+    "revision": "c347a5d08e65d5e7c359",
+    "url": "/static/js/2.c347a5d0.chunk.js"
+  },
+  {
+    "revision": "73e01ec8a6d1b1de3d73ae8798e3a898",
+    "url": "/index.html"
+  }
+];
